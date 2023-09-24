9b20c2683fab3f1f3d2b0e464e21ceef8c80c4f2
Umut Oeztuerk <ozturk_umut02@hotmail.com>
2023-09-25 00:15:16 +0200

    Init2

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainContainer from './navigation/MainContainer'
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <MainContainer/>
+  );
+};
+export default App;
