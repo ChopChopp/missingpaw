84e8f59f6f7d6f57f784d03d934642c3452d1a50
Umut Oeztuerk <ozturk_umut02@hotmail.com>
2023-11-19 20:29:31 +0100

    MyPet implementation

--- app/screens/main/home/yourPet/YourPet.tsx
+++ app/screens/main/home/yourPet/YourPet.tsx
@@ -1,17 +1,103 @@
 import * as React from "react";
-import {SafeAreaView} from "react-native";
+import {Image, SafeAreaView, StyleSheet, TouchableOpacity, View} from "react-native";
 import ThemedText from "../../../../helper/themedText/ThemedText";
+import {FIREBASE_DATABASE} from "../../../../../FirebaseConfig";
+import {ref, get} from "firebase/database";
+import {useEffect, useState} from "react";
+import AddPet from "./addPet/AddPet";
+
+interface Pet {
+    name: string;
+    age: number;
+    type: string;
+    breed: string;
+    color: string;
+    imageUrl: string;
+}
 
 const YourPet = ({userData}: any) => {
-    console.log(userData)
-    console.log(userData.email)
+    const [pet, setPet] = useState<Pet | null>(null);
+    const [hasPets, setHasPets] = useState<boolean | null>(null);
+    const [showAddPet, setShowAddPet] = React.useState<boolean>(false);
+
+    const userRef = ref(FIREBASE_DATABASE, "users/" + userData.id + "/pet");
+
+    useEffect(() => {
+        const checkForPets = () => {
+            get(userRef).then(snapshot => {
+                if (snapshot.exists()) {
+                    console.log('User already has pets:', snapshot.val());
+                    setPet(snapshot.val());
+                    setHasPets(true);
+                } else {
+                    console.log('User does not have any pets.');
+                    setHasPets(false);
+                }
+            }).catch((error) => {
+                console.error('Error checking for pets:', error);
+                setHasPets(false);
+            });
+        };
+
+        checkForPets();
+    }, [showAddPet]);
+
+
     return (
         <SafeAreaView>
-            <ThemedText>
-                YourPet
-            </ThemedText>
+            {hasPets
+                ?
+                <>
+                    {pet && (
+                        <>
+                            <ThemedText>You already have a pet!</ThemedText>
+                            <ThemedText>{pet.name}</ThemedText>
+                            <ThemedText>{pet.age}</ThemedText>
+                            <ThemedText>{pet.breed}</ThemedText>
+                            <ThemedText>{pet.type}</ThemedText>
+                            <ThemedText>{pet.color}</ThemedText>
+                            <Image source={{uri: pet.imageUrl}} style={{width: 200, height: 200}}/>
+                        </>
+                    )}
+                </>
+
+                :
+                showAddPet ?
+                    <AddPet userData={userData} setShowAddPet={setShowAddPet} />
+                    :
+                    <View style={styles.container}>
+                        <ThemedText>You don't have a pet yet!</ThemedText>
+                        <TouchableOpacity style={styles.button} onPress={() => setShowAddPet(true)}>
+                            <ThemedText>Click here to add your pet</ThemedText>
+                        </TouchableOpacity>
+                    </View>
+
+            }
         </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection:
+            "column",
+        alignItems:
+            'center',
+        justifyContent:
+            "center",
+    }
+    ,
+    button: {
+        paddingHorizontal: 10,
+        marginHorizontal:
+            5,
+        backgroundColor:
+            "#8e8e93",
+        borderRadius:
+            5,
+    }
+});
+
 
 export default YourPet;
---
+++ app/screens/main/home/yourPet/addPet/AddPet.tsx
@@ -0,0 +1,186 @@
+import * as React from "react";
+import {
+    ActivityIndicator,
+    SafeAreaView,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity, useColorScheme,
+    View
+} from "react-native";
+import ThemedText from "../../../../../helper/themedText/ThemedText";
+import {ref, set} from "firebase/database";
+import {FIREBASE_DATABASE} from "../../../../../../FirebaseConfig";
+import {useState} from "react";
+import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
+import {DarkTheme, LightTheme} from "../../../../../helper/theme/Theme";
+
+const YourPet = ({userData, setShowAddPet}: any) => {
+    const userRef = ref(FIREBASE_DATABASE, "users/" + userData.id + "/pet");
+    const textColor = useColorScheme() === 'dark' ? DarkTheme.colors.text : LightTheme.colors.text;
+
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [type, setType] = useState("");
+    const [breed, setBreed] = useState("");
+    const [color, setColor] = useState("");
+    const [image, setImage] = useState("");
+
+    const [loading, setLoading] = useState(false);
+
+    const petObject = {
+        name: name,
+        age: age,
+        type: type,
+        breed: breed,
+        color: color,
+        imageUrl: "https://random.dog/b780466a-d547-45db-9168-924e3dc485ea.jpg"
+    };
+
+    const createPetObject = () => {
+        if (name === "" || age === "" || type === "" || breed === "" || color === "" || image === "") {
+            alert("Please fill out all fields!");
+            return;
+        }
+
+        console.log("Creating pet object...");
+        setLoading(true)
+
+        set(userRef, petObject).then(() => {
+            console.log("Pet object added successfully!")
+            setLoading(false)
+            setShowAddPet(false)
+        }).catch((error) => {
+            console.log("Failed to add pet object:", error);
+        });
+    }
+
+    return (
+        <SafeAreaView style={{flex: 1, width: '100%'}}>
+            <View style={{alignItems: "flex-start"}}>
+                <TouchableOpacity style={styles.backButton} onPress={() => setShowAddPet(false)}>
+                    <ThemedText>Back</ThemedText>
+                </TouchableOpacity>
+            </View>
+            <KeyboardAwareScrollView
+                style={styles.mainContainer}
+                contentContainerStyle={styles.inner}
+                resetScrollToCoords={{x: 0, y: 0}}
+                scrollEnabled={true}
+                keyboardOpeningTime={0}
+            >
+                <ThemedText style={styles.title}>Pet details</ThemedText>
+                <TextInput
+                    style={[styles.input, { color: textColor }]}
+                    placeholder="Pet name"
+                    value={name}
+                    onChangeText={(text) => setName(text)}
+                    autoCapitalize="none"
+                    keyboardType="default"
+                />
+                <TextInput
+                    style={[styles.input, { color: textColor }]}
+                    placeholder="Age"
+                    value={age}
+                    onChangeText={(text) => setAge(text)}
+                    autoCapitalize="none"
+                    keyboardType="numeric"
+                />
+                <TextInput
+                    style={[styles.input, { color: textColor }]}
+                    placeholder="Pet type (Dog, Cat, etc.)"
+                    value={type}
+                    onChangeText={(text) => setType(text)}
+                    autoCapitalize="none"
+                    keyboardType="default"
+                />
+                <TextInput
+                    style={[styles.input, { color: textColor }]}
+                    placeholder="Breed (Bernese Mountain Dog, etc.)"
+                    value={breed}
+                    onChangeText={(text) => setBreed(text)}
+                    autoCapitalize="none"
+                    keyboardType="default"
+                />
+                <TextInput
+                    style={[styles.input, { color: textColor }]}
+                    placeholder="Color (Black, Brown, etc.)"
+                    value={color}
+                    onChangeText={(text) => setColor(text)}
+                    autoCapitalize="none"
+                    keyboardType="default"
+                />
+                <TextInput
+                    style={[styles.input, { color: textColor }]}
+                    placeholder="Upload image..."
+                    value={image}
+                    onChangeText={(text) => setImage(text)}
+                    autoCapitalize="none"
+                    keyboardType="default"
+                />
+
+                <View style={styles.actionContainer}>
+                    {loading ? (
+                        <ActivityIndicator size="large" color="#0000ff"/>
+                    ) : (
+                        <>
+                            <TouchableOpacity style={styles.submitButton} onPress={() => createPetObject()}>
+                                <ThemedText>Add pet</ThemedText>
+                            </TouchableOpacity>
+                        </>
+                    )}
+                </View>
+            </KeyboardAwareScrollView>
+
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    backButton: {
+        backgroundColor: "#8e8e93",
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        marginRight: 250,
+        marginTop: 10,
+    },
+    submitButton: {
+        backgroundColor: "#34c759",
+        borderRadius: 5,
+        paddingHorizontal: 10,
+    },
+    mainContainer: {
+        flex: 1,
+        paddingHorizontal: 10,
+        marginHorizontal: 5,
+    },
+    inner: {
+        flexGrow: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 16,
+    },
+    input: {
+        width: "80%",
+        height: 48,
+        padding: 12,
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 4,
+        marginBottom: 16,
+        backgroundColor: "#1c1c1e",
+        minWidth: "90%",
+        color: "#fff",
+    },
+    actionContainer: {
+        height: 100,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+});
+
+
+export default YourPet;
