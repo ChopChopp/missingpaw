e3ecdc7ae6977de2a8da066345aa2902c55c3685
Umut Oeztuerk <ozturk_umut02@hotmail.com>
2024-01-22 22:44:55 +0100

    Firebase prod config change

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,29 +1,32 @@
 import {initializeApp} from "firebase/app";
+import {getAnalytics, isSupported} from "firebase/analytics";
 import {getAuth} from "firebase/auth";
 import {getDatabase} from "firebase/database";
 import {initializeAuth, getReactNativePersistence} from 'firebase/auth';
-import { getStorage } from "firebase/storage";
+import {getStorage} from "firebase/storage";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB0CtLKz2yZ4x4heTtHKQ0ns3nX-AxiEaw",
-    authDomain: "simao-project-pioneer.firebaseapp.com",
-    databaseURL: "https://simao-project-pioneer-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "simao-project-pioneer",
-    storageBucket: "simao-project-pioneer.appspot.com",
-    messagingSenderId: "832432520804",
-    appId: "1:832432520804:web:56470e42108f3269952971",
-    measurementId: "G-PGQKP2S4K2"
+    apiKey: "AIzaSyCy6RE6Y5zoFe5rHOkwOWpNAzUBfCypBZk",
+    authDomain: "missing-paw.firebaseapp.com",
+    databaseURL: "https://missing-paw-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "missing-paw",
+    storageBucket: "missing-paw.appspot.com",
+    messagingSenderId: "80490557072",
+    appId: "1:80490557072:web:1675414e46fc36f982f215",
+    measurementId: "G-HSP1BZC9WD"
 };
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig,);
+const analytics = isSupported().then(yes => yes ? getAnalytics(FIREBASE_APP) : null);
 
-// Create a root reference
+// Create a root reference.
 export const STORAGE = getStorage();
 
 const auth = initializeAuth(FIREBASE_APP, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
+
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
 export const FIREBASE_DATABASE = getDatabase();
