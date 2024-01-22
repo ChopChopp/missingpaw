18f9805a8e973ee54f8e116f623db82e109b3ac0
Umut Oeztuerk <ozturk_umut02@hotmail.com>
2024-01-22 22:58:54 +0100

    Cleanup settings page

--- app/screens/main/settings/Settings.tsx
+++ app/screens/main/settings/Settings.tsx
@@ -1,63 +1,58 @@
 import * as React from "react";
 import {
     View,
-    Text,
     Button,
     ActivityIndicator,
-    StyleSheet, useColorScheme,
+    StyleSheet,
 } from "react-native";
 import {getAuth, signOut} from "firebase/auth";
-import {DarkTheme, LightTheme} from "../../../helper/theme/Theme";
+import ThemedText from "../../../helper/themedComponents/themedText/ThemedText";
 
 const auth = getAuth();
 
-const Settings = ({navigation, userData}: any) => {
-    const textColor = useColorScheme() === 'dark' ? DarkTheme.colors.text : LightTheme.colors.text;
-
+const Settings = ({userData}: any) => {
     if (!userData) {
         return <ActivityIndicator size="large" color="#0000ff"/>;
     }
 
-    const styles = StyleSheet.create({
-        text: {
-            fontSize: 16,
-            marginBottom: 8,
-            color: textColor
-        },
-        title: {
-            fontSize: 26,
-            fontWeight: "bold",
-            color: textColor
-        }
-    });
-
     return (
         <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-            <Text
-                onPress={() => navigation.navigate("Home" as never)}
-                style={styles.title}
-            >
-                从上图e拉走他😎
-                Settings
-            </Text>
+            <ThemedText style={styles.title}>Settings</ThemedText>
+            <View style={styles.details}>
+                <ThemedText style={styles.text}>Email: {userData.email}</ThemedText>
+                <ThemedText style={styles.text}>First Name: {userData.firstName}</ThemedText>
+                <ThemedText style={styles.text}>Last Name: {userData.lastName}</ThemedText>
+            </View>
             <Button
                 title={"Sign out"}
                 onPress={() =>
                     signOut(auth)
                         .then(() => {
-                            alert("Sign out successful!");
+                            console.info("Sign out successful!");
                         })
                         .catch((error) => {
-                            alert("Sign out failed: " + error);
+                            console.error("Sign out failed: " + error);
                         })
                 }
             />
-            <Text style={styles.text}>ID: {userData.id}</Text>
-            <Text style={styles.text}>Email: {userData.email}</Text>
-            <Text style={styles.text}>First Name: {userData.firstName}</Text>
-            <Text style={styles.text}>Last Name: {userData.lastName}</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 16,
+        marginBottom: 8,
+    },
+    title: {
+        fontSize: 26,
+        fontWeight: "bold",
+    },
+    details: {
+        width: "80%",
+        marginTop: 16,
+        marginBottom: 16,
+    },
+});
 
 export default Settings;
